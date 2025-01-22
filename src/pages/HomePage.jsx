87964378c8514703d1b0a583c9085683c2638fe6
NANDHINI7390 <139752584+NANDHINI7390@users.noreply.github.com>
2025-01-22 13:54:16 +0530

    Initial commit

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Navbar from "../components/Navbar";
+
+const HomePage = () => {
+  return (
+    <div>
+      <Navbar />
+      <header>
+        <h1>Welcome to [Your Coffee Shop]</h1>
+      </header>
+    </div>
+  );
+};
+
+export default HomePage;
